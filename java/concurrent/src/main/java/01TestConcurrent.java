2dff2c2cbf1c79c9bb43ebf568802ab284e8e018
Chris <chrisyupengwen@gmail.com>
2024-05-31 10:50:33 +1000

    20240531 并发编程01

---
+++ java/concurrent/src/main/java/01TestConcurrent.java
@@ -0,0 +1,27 @@
+
+public class Test {
+    private long count = 0;
+
+    private void add10K() {
+        int idx = 0;
+        while (idx < 10000) {
+            count += 1;
+        }
+    }
+}
+
+public static long calc() {
+    final Test test = new Test();
+    Thread thread1 = new Thread(() -> {
+        test.add10K();
+    });
+    Thread thread2 = new Thread(() -> {
+        test.add10K();
+    });
+    thread1.start();
+    thread2.start();
+    //等待两个线程结束
+    thread1.join();
+    thread2.join();
+    return test.count;
+}
