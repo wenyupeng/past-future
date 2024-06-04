b2e0ca5dc026fc28d1e46c181e522d211b0a8522
Chris <chrisyupengwen@gmail.com>
2024-06-04 10:04:33 +1000

    20240604 并发编程02

--- java/concurrent/src/concurrent/java/test01/TestConcurrent_01.java
+++ java/concurrent/src/concurrent/java/test01/TestConcurrent_01.java
@@ -1,27 +1,25 @@
+package concurrent.java.test01;
 
-public class Test {
+public class TestConcurrent_01 {
     private long count = 0;
 
     private void add10K() {
         int idx = 0;
         while (idx < 10000) {
             count += 1;
+            idx++;
         }
     }
-}
 
-public static long calc() {
-    final Test test = new Test();
-    Thread thread1 = new Thread(() -> {
-        test.add10K();
-    });
-    Thread thread2 = new Thread(() -> {
-        test.add10K();
-    });
+public long calc() throws InterruptedException {
+    final TestConcurrent_01 test = new TestConcurrent_01();
+    Thread thread1 = new Thread(test::add10K);
+    Thread thread2 = new Thread(test::add10K);
     thread1.start();
     thread2.start();
     //等待两个线程结束
     thread1.join();
     thread2.join();
     return test.count;
+}
 }
