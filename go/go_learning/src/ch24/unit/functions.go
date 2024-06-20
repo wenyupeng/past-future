d9b94ba64340b468e5ed1b8287571c1cb527514a
Chris <chrisyupengwen@gmail.com>
2024-06-20 11:27:39 +0800

    20240620 删除仓库

--- go/go_learning/src/ch24/unit/functions.go
+++
@@ -1,5 +0,0 @@
-package unit
-
-func square(op int) int {
-	return op * op
-}
--- nodejs/code/01hello.js
+++
@@ -1,9 +0,0 @@
-console.log('hello Node.js')
-
-// build a server
-var http =require('http')
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hello Node.js')
-}).listen(8080)
