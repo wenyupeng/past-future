3f2eca10d77175a7d3ed3ddec1b2b1c972c89fe6
Chris <chrisyupengwen@gmail.com>
2024-07-25 13:55:19 +1000

    20240725 sit771

---
+++ deakin/sit771/2.3C/RobotDodge/Player.cs
@@ -0,0 +1,53 @@
+using SplashKitSDK;
+
+
+namespace RobotDodge
+{
+    public class Player
+    {
+        private Bitmap _PlayerBitmap;
+        private Window _GameWindow;
+
+        public double X
+        {
+            get;
+            private set;
+        }
+
+        public double Y
+        {
+            get;
+            private set;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _PlayerBitmap.Width;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _PlayerBitmap.Height;
+            }
+        }
+
+        public Player(Window gameWindow)
+        {
+            _PlayerBitmap = new Bitmap("User", "Player.png");
+            X = (gameWindow.Width - Width) / 2;
+            Y = (gameWindow.Height - Height) / 2;
+            // Console.WriteLine($"BitMap Width {Width}, Height {Height}; X {X}, Y {Y}");
+            _GameWindow = gameWindow;
+        }
+
+        public void Draw()
+        {
+            _GameWindow.DrawBitmap(_PlayerBitmap, X, Y);
+        }
+    }
+}
---
+++ deakin/sit771/2.3C/RobotDodge/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using SplashKitSDK;
+
+namespace RobotDodge
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window gameWindow = new Window("RobotDodge created by Chris 224212855", 800, 800);
+            gameWindow.Clear(Color.White);
+
+            gameWindow.Refresh(50);
+            Player player = new Player(gameWindow);
+
+            player.Draw();
+            gameWindow.Refresh(50);
+            SplashKit.Delay(10000);
+
+        }
+    }
+}
