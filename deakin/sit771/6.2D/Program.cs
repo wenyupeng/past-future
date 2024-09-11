e5d2b28f4d5899c72223669acfc53164edc2d7e9
Chris <chrisyupengwen@gmail.com>
2024-09-11 15:13:34 +1000

    20240911 sit773

--- deakin/sit771/6.2D/Program.cs
+++ deakin/sit771/6.2D/Program.cs
@@ -3,16 +3,33 @@ using SplashKitSDK;
 
 namespace _6_2D
 {
+    delegate void Procedure();
     public class Program
     {
+        public static void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("Method 3");
+        }
         public static void Main()
         {
             Window? gameWindow = new Window("RobotDodge created by Chris 224212855", 800, 800);
 
-            RobotDodge robotDodge=new RobotDodge(gameWindow);
+            RobotDodge robotDodge = new RobotDodge(gameWindow);
 
-            while(!gameWindow.CloseRequested){
-                if(robotDodge.Quit){
+            while (!gameWindow.CloseRequested)
+            {
+                if (robotDodge.Quit)
+                {
                     break;
                 }
                 robotDodge.HandleInput();
@@ -22,6 +39,20 @@ namespace _6_2D
 
             gameWindow.Close();
             gameWindow = null;
+        }
+
+        public static void TestDelegate()
+        {
+            Procedure someProcs = null;
+
+            someProcs += new Procedure(Program.Method1);
+            someProcs += Program.Method1;
+            someProcs += new Procedure(Method2);  // Example with omitted class name
+
+            Program demo = new Program();
+
+            someProcs += new Procedure(demo.Method3);
+            someProcs();
         }
     }
 }
